bdcd13ae28c1f99fec02a781894e123be8853baa
imransaglam <imransaglam9@gmail.com>
2023-03-04 20:01:23 +0300

    initial commit

---
+++ Repositories/EFCore/RepositoryManger.cs
@@ -0,0 +1,23 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public class RepositoryManger : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+
+        public RepositoryManger(RepositoryContext context)
+        {
+            _context = context; 
+        }
+
+        public IBookRepository Book => new BookRepository(_context);
+
+        public void Save()=>_context.SaveChanges();
+    }
+}
